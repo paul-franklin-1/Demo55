71d28eaa272f1fcf8d86314822f86a6801247416
Paul Franklin <paul.franklin@csuglobal.edu>
2024-04-06 19:55:19 -0400

    fixing up the buttons

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -1,54 +1,69 @@
-
-import com.sun.prism.Graphics;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import java.util.Random;
-import javafx.scene.text.Text;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.control.Button;
-public static class LemonadeStand extends Application {
+public class LemonadeStand extends Application {
     int colorIntA;
     int colorIntB;
     int colorIntC;
     public void start(Stage lemonStage){
         BorderPane lemonRoot = new BorderPane();
-        Scene lemonScene = new Scene(lemonRoot, 850,500);
+        Scene lemonScene = new Scene(lemonRoot, 1200,800);
         lemonStage.setScene(lemonScene);
-        lemonStage.show();}}
+        lemonStage.setTitle("Chicago Lemonade Stand");
+        Button dateBttn = new Button("View Today's Date");
+        dateBttn.setFont(new Font("Arial", 30));
+        Button dwnldBttn = new Button("Download as Text File");
+        dwnldBttn.setFont(new Font("Arial", 30));
+        Button randColorBttn = new Button("Set Random Color");
+        randColorBttn.setFont(new Font("Arial", 30));
+        Button quitBttn = new Button("Quit Program");
+        quitBttn.setFont(new Font("Arial", 30));
+        HBox lemonHBox = new HBox();
+        lemonHBox.getChildren().addAll(dateBttn, dwnldBttn, randColorBttn, quitBttn);
+        lemonRoot.setTop(lemonHBox);
+        lemonHBox.setAlignment(Pos. CENTER);
+        dateBttn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("the date is 3/22/1888");
+            }});
+
+        dwnldBttn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+               System.out.println("download complete ya jackass");
+            }});
+        randColorBttn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("a color a color colr!");}});
+
+        quitBttn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Program is not quitting, be patient...");
+            }});
+        lemonStage.show();}
 
         /*GridPane lemonGrid = new GridPane();
         lemonGrid.setPrefSize(1200, 850);
-        Stage lemonStage = new Stage();
-        lemonStage.setWidth(1200);
-        lemonStage.setHeight(850);
-        lemonStage.setTitle("Suburban Lemonade Stand");
-        Button dateBttn = new Button("Show Current Date");
-        Button dwnldBttn = new Button("Download as text file");
-        Button randColorBttn = new Button("Random Color");
-        Button quitBttn = new Button("Quit Program");
-        lemonGrid.setHgap(14);
-        lemonGrid.setVgap(18);
-        Random rand = new Random();
-        Pane lemonPane = new Pane();
-        lemonPane.setPrefSize(500,400);
-        lemonPane.getChildren().add(dateBttn);
-        lemonPane.getChildren().add(dwnldBttn);
-        lemonPane.getChildren().add(randColorBttn);
-        lemonPane.getChildren().add(quitBttn);
-        Scene lemonScene = new Scene(lemonPane, 700, 400);
-        colorIntA = rand.nextInt(256);
-        colorIntB = rand.nextInt(256);
-        colorIntC = rand.nextInt(256);
-        Color randoColor = Color.rgb(colorIntA, colorIntB, colorIntC);
-        lemonScene.setFill(randoColor);
-        lemonStage.setScene(lemonScene);
-        lemonStage.show(); }*/
+
+
+        ///// Random rand = new Random();
+        /////Scene lemonScene = new Scene(lemonPane, 700, 400);
+        /////colorIntA = rand.nextInt(256);
+        /////colorIntB = rand.nextInt(256);
+       ///// colorIntC = rand.nextInt(256);
+       ///// Color randoColor = Color.rgb(colorIntA, colorIntB, colorIntC);
+       ///// lemonScene.setFill(randoColor);
+*/
     public static void main(final String[] args) {
         launch(args); }
 
